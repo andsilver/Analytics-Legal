ef39bee8aba71075e1dc17de8d28a79d2d87f9f9
Eli <elicloudh@gmail.com>
2018-06-21 23:40:41 +0300

    Create List of Users page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,9 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
 
   def create
     @user = User.new(user_params)
@@ -22,13 +27,21 @@ class Admin::UsersController < ApplicationController
   def update
   end
 
-  def delete
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_users_path, notice: 'Blog was successfully deleted.' }
+    end
   end
 
   def show
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :role, :company, :company_id, :address, :country,
