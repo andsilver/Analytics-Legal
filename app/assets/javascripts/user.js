dd7d1bb1751d256fc160513a5b2afce2c5d14380
Eli <elicloudh@gmail.com>
2018-06-27 23:54:56 +0200

    Set minimal input length for rut search

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('.allowed-ruts-select2').select2({
     placeholder: 'RUT permitidas',
+    minimumInputLength: 3,
     ajax: {
       url: '/laboral/litigants/search',
       dataType: 'json'
