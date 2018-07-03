b4462f880e677c25b39cdd486f8e375eb6684e26
Eli <elicloudh@gmail.com>
2018-07-03 18:07:49 +0200

    Update data for pie chart with Top 10 Materias every 15 minutes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, "/var/www/deeplegal/shared/log/cron_log.log"
 
-every 12.hours do
+every 15.minutes do
   runner "Cache::MattersCache.new(Logger.new('/var/www/deeplegal/shared/log/matters_cache_cron.log')).save_all"
 end
