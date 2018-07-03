2835635008cee2b51102f0fc177c13935176b0f6
Eli <elicloudh@gmail.com>
2018-07-03 19:21:16 +0200

    Add pie chart index page for users with no ruts

--- app/controllers/laboral/matters_controller.rb
+++ app/controllers/laboral/matters_controller.rb
@@ -6,10 +6,14 @@ class Laboral::MattersController < ApplicationController
 
     @matters = Cache::MattersCache.new.get(current_user.id)
     @matters_updated_at = Cache::MattersCache.new.get_updated_at(current_user.id).to_i
+
+    template = @matters.empty? ? "index_no_ruts" : "index"
+
+    render template
   end
 
   def update_cache
-    Cache::MattersCache.new.save(current_user)
+    MattersCacheWorker.perform_async(current_user.id)
 
     redirect_to laboral_matters_path
   end
