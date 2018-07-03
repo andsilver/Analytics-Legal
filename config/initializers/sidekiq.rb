132d5e353fee26d967ca851c4ed0e25554e6f218
Eli <elicloudh@gmail.com>
2018-07-03 19:48:52 +0200

    Add workaround to disabled redis CLIENT command

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: Rails.application.secrets.redis_url || "redis://localhost:6379" }
+  config.redis = { url: Rails.application.secrets.redis_url || "redis://localhost:6379", id: nil }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.application.secrets.redis_url || "redis://localhost:6379" }
+  config.redis = { url: Rails.application.secrets.redis_url || "redis://localhost:6379", id: nil }
 end
