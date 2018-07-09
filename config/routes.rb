43223cb05cb92dc641808bbbded161ccb0d1bb4d
Eli <elicloudh@gmail.com>
2018-07-10 00:59:59 +0300

    Add user authentication for accessing Sidekiq UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
+  authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
+  end
 
   namespace :admin do
     resources :users
