16b9725011460897320c8a6ca39a2bc17ca274df
Eli <elicloudh@gmail.com>
2018-07-14 00:31:22 +0200

    Filter repeated ruts in js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -16,10 +16,17 @@ $(document).ready(function() {
         return query;
       },
       processResults: function (data, params) {
-        params.page = params.page || 1;
+        existingRuts = _.map($('.allowed-ruts-select2').select2('data'), function(el) {
+          return el.id.split(':')[1];
+        });
 
+        params.page = params.page || 1;
         return {
-          results: data.results,
+          results: _.filter(data.results, function(el) {
+            rut = el.id.split(':')[1];
+
+            return rut == '0-0' || !existingRuts.includes(rut);
+          }),
           pagination: {
             more: data.pagination.more
           }
