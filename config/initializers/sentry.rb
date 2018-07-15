4187a5137c5af088cec86cc23c9cec5cd2160342
Eli <elicloudh@gmail.com>
2018-07-15 03:08:53 +0200

    Add sentry initializer

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,4 @@
+Raven.configure do |config|
+  config.dsn = 'https://4818928d18ab42fa8369fcb47dc86920:8f73779d217d494c8bb27195b38ea5c8@sentry.io/1243406'
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+end
